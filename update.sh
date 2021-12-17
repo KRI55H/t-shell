67b6ef5a2ffa1448f1c0b4d96b0ad447b6103acf
Kriss siddhapura <65755721+siddhapurakriss@users.noreply.github.com>
2021-12-17 12:16:19 +0530

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,38 @@
+#! /data/data/com.termux/files/usr/bin/bash
+#####################################
+# PROJECT NAME : T-SHELL            #
+# PROJECT BY : SIDDHAPURA KRISS     #
+# CONTECT : https://t.me/kalilinux7 #
+#####################################
+clear
+
+echo -e "\e[32m█████████████████████████████████████████████████████████████"
+echo -e "\e[32m██                                                         ██"
+echo -e "\e[32m██  ████████      ███████ ██   ██ ███████ ██      ██       ██"
+echo -e "\e[32m██     ██         ██      ██   ██ ██      ██      ██       ██"
+echo -e "\e[32m██     ██   █████ ███████ ███████ █████   ██      ██       ██"
+echo -e "\e[32m██     ██              ██ ██   ██ ██      ██      ██       ██"
+echo -e "\e[32m██     ██         ███████ ██   ██ ███████ ███████ ███████  ██"
+echo -e "\e[32m██                                                         ██"
+echo -e "\e[32m████████████████████████████████████████████████ -V 3.0 █████"
+echo ||lolcat -t
+sleep 1
+echo -e "\e[31m +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo -e "\e[31m +\e[36m SCRIPT BY : SIDDHAPURA KRISS    - VERSION 3.0\e[31m +"
+echo -e "\e[31m +\e[36m SETUP YOUR SHELL LIKE PRO                    \e[31m +"
+echo -e "\e[31m +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo ||lolcat -t
+sleep 2
+echo -e "\e[32m[*]\e[34m Updating current tool ... \e[m "||lolcat -t
+sleep 3
+echo
+
+cd
+rm -rf t-shell
+git clone https://github.com/siddhapurakriss/t-shell
+sleep 3
+echo
+echo -e "\033[1;34m[*]\e[33m Update Finished. Checkout new changes! \e[m"||lolcat -t
+ls
+cd t-shell
+ls
